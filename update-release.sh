16a0d0e913a6b0af4445e2790f9e0a9c58a94f15
gustavo lorenzo <gustavonlorenzo@gmail.com>
2023-07-10 13:55:44 -0400

    t

--- update-release.sh
+++ update-release.sh
@@ -5,9 +5,7 @@ set -feu	# Usage: $0 <RELEASE_VERSION>	# Fails when RELEASE_VERSION is not provi
 
 RELEASE_VERSION=$RELEASE_VERSION
 echo $RELEASE_VERSION
-sed -i "s|image: ghcr.io/gustavonlorenzo/pytest:.*|image: ghcr.io/gustavonlorenzo/pytest:$RELEASE_VERSION|" ./clusters/my-cluster/pytest/deployment.yml
+sed -i "s|image: ghcr.io/gustavonlorenzo/pytest:.*|image: ghcr.io/gustavonlorenzo/pytest:$RELEASE_VERSION|" /opt/actions-runner/_work/my-flux-repo/my-flux-repo/clusters/my-cluster/pytest/deployment.yml
 
-cat ./clusters/my-cluster/pytest/deployment.yml
-
-git add ./clusters/my-cluster/pytest/deployment.yml
+cat /opt/actions-runner/_work/my-flux-repo/my-flux-repo/clusters/my-cluster/pytest/deployment.yml
 
