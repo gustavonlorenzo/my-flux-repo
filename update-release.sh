1ebe4b8295e819f48ce65c69d5857484ac36d057
gustavo lorenzo <gustavonlorenzo@gmail.com>
2023-07-10 12:05:05 -0400

    t

--- update-release.sh
+++ update-release.sh
@@ -3,6 +3,7 @@
 # update-k8s.sh
 set -feu	# Usage: $0 <RELEASE_VERSION>	# Fails when RELEASE_VERSION is not provided
 
-RELEASE_VERSION=${RELEASE_VERSION}
+RELEASE_VERSION=$RELEASE_VERSION
+echo $RELEASE_VERSION
 sed -i "s|image: ghcr.io/gustavonlorenzo/pytest:.*|image: ghcr.io/gustavonlorenzo/pytest:$RELEASE_VERSION|" ./clusters/my-cluster/pytest/deployment.yml
 
